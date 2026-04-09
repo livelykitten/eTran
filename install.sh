0bf6ad11e4315cd575c834c9daea87ffe858ff18
Kyoung Wan Min <ichupu9999@gmail.com>
2026-04-09 00:49:05 +0000

    update clang to 17 (16 is deprecated)

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ sudo cp /usr/local/sbin/bpftool /usr/local/bin/
 popd > /dev/null
 rm -rf bpftool/
 
-# llvm-16
+# llvm-17
 wget https://apt.llvm.org/llvm.sh
 chmod u+x llvm.sh
-sudo ./llvm.sh 16
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-16 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-16 100
-sudo update-alternatives --install /usr/bin/llc llc /usr/bin/llc-16 100
+sudo ./llvm.sh 17
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-17 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-17 100
+sudo update-alternatives --install /usr/bin/llc llc /usr/bin/llc-17 100
